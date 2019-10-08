68ecd29bb90f6f00f562c74e330a20cf446e666f
Daniel Basten <axhm3a@gmail.com>
2019-10-08 11:11:38 +0200

    add ui

---
+++ Assets/Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.UNetWeaver;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public UnityEngine.UI.Button button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        Debug.Log("onClick");
+    }
+}
